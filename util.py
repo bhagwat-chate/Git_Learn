3f7d03ebdf2390e2a7059d969247257d4e0673f4
bhagwat-chate <bhagwat.chate25@gmail.com>
2022-10-05 12:26:36 +0530

    utils.py modified

--- util.py
+++ util.py
@@ -2,6 +2,8 @@ class supportOps:
     def __init__(self):
         pass
 
-    def supportfunction(self):
-        print('Hello! from support function.')
+    def supportfunction1(self):
+        print('Hello! from support function 1')
 
+    def supportfunction2(self):
+        print("Hello! from support function 2")
